596d85bd635c19a2eb46926f2591741015df72c1
nizamibilal1064 <nizamibilal1064@gmail.com>
2017-07-15 11:53:55 +0200

    pca update

--- pca.py
+++ pca.py
@@ -28,17 +28,23 @@ from sklearn import preprocessing
 print '\n\n'
 print '|=======================================================|'
 print '|\t\t\t\t\t\t\t|'
-print '|\t\t :-) PCA MD (-:	\t\t\t|\n','|\t\t\t\t\t\t\t|\n', '|\t\twritten by Bilal Nizami\t\t\t|\n', '|\t\tRhodes University, 2017\t\t\t|'
-print '|\t    distributed under GNU GPL 3.0\t\t|'
+print '|\t :-) >>-----------> PCA MD <-----------<< (-:	|'
+print '|\t\t\t\t\t\t\t|'
+print '|\t\t\t\t\t\t\t|'
+print '|   This programe performs the PCA (Principal Component\t| \n|             Analysis) on a MD trajectory\t\t|'
+print '|\t\t\t\t\t\t\t|\n', '|\tAuthors:  Bilal Nizami\t\t\t\t|\n','|\tResearch Unit in Bioinformatics (RUBi)\t\t|\n', '|\tRhodes University, 2017\t\t\t\t|'
+print '|\tDistributed under GNU GPL 3.0\t\t\t|'
+print '|\t\t\t\t\t\t\t|'
+print '|\thttps://github.com/michaelglenister/NMA-TASK\t|'
 print '|\t\t\t\t\t\t\t|'
 print '|=======================================================|'
 print '\n'
 
 #==============================================================================
-#							   Setting the options
+#                            Setting the options
 #==============================================================================
 
-parser = OptionParser("Usage: pca.py -t <MD trajecotry> -p <topology file>  -a <atom group >")
+parser = OptionParser("Usage: pca.py -t <MD trajectory> -p <topology file>  -a <atom group >")
 
 parser.add_option("-t", "--trj", type='string', dest="trj",
                   help="file name of the MD trajectory")
@@ -47,12 +53,20 @@ parser.add_option("-p", "--top", type='string', dest="topology",
                   help="topology file")      
 
 parser.add_option("-a", "--ag", type='string', dest="atm_grp",
-                  help="group of atom for PCA. Options are:\
-				  all= all atoms, backbone = backbone atoms, CA= C alpha atoms\
-				  protein= protein's atoms")				  
+                  help="group of atom for PCA. Default is C alpha atoms. Other options are:\
+				  all= all atoms\
+				  backbone = backbone atoms\
+				  CA= C alpha atoms\
+				  protein= protein's atoms")	
+
+parser.add_option("-r", "--ref", type='string', dest="reference",
+                  help="reference structure for RMSD") 
 
 (options, args) = parser.parse_args()
                      
+atm_name = options.atm_grp
+print "Atom group selected for PCA:", atm_name, "\n"
+
 
 #====================================================================
 # if no arguments are passed
@@ -67,72 +81,111 @@ if options.topology is None:
 	parser.print_help()
 	sys.exit(1)
 
-# assign the passed arguments
+#=======================================
+# assign the passed arguments and read the trajectory 
+#=======================================
+
 traj = options.trj
 topology = options.topology
+ref = options.reference
+if ref:
+	ref = md.load(options.reference)
 
-
-# read the trajecotry 
 pca_traj = md.load(traj, top=topology)
 #print(pca_traj)
 top = pca_traj.topology
 #print top
+
+#==============================================
+#
+# Setting the default options
+#
+#===============================================
+
+if options.atm_grp is None:
+	print 'No atom has been selected. PCA will be performed on C alpha atoms '
+	atm_name = 'CA'  # set to default C-alpha atoms
+if options.reference == None:
+	print "No reference structure given, RMSD will be computed to the first frame in the trajectory"
+	ref = pca_traj # set reference to current trajectory
+
+
 #==========================================================================
 #
 #		selecting the atoms 
 #
 # User passes the arguements to select the subset of atoms for PCA
 #===========================================================================
-if options.atm_grp is None:
-	print 'No atom group selected. Please select a atom group for PCA analysis\n'
-	parser.print_help()
-	sys.exit(1)
-	
-atm_name = options.atm_grp
-print "Group selected for PCA:", atm_name, "\n"
 
 
 def get_trajectory():
+	'get the part of system for PCA based on users input of atom group'
 	if atm_name == 'CA':
 		sele_grp=top.select("name CA")	
-	
+
 	if atm_name == 'backbone':
 		sele_grp=top.select("backbone")
+		
 	if atm_name == 'all':
 		sele_grp=top.select("all")
 	return sele_grp;
 	
+	if atm_name == 'protein':
+		sele_grp=top.select("protein")
+	return sele_grp;
 
 sele_grp = get_trajectory()
-print len(sele_grp)
-#sele_trj = pca_traj.xyz[:,sele_grp,:]
-#print "Number of frames in the trajectory selected for PCA:", len(sele_trj), "\n"
 
+# print trajectory informations
+def trajectory_info():
+	'Prints various information of MD trajectory'
+	print '\n\nTrajectory info:\n'
+	print "Total",pca_traj.n_frames,"frames read from", traj
+	print "MD time is from ", pca_traj.time[0],'to',pca_traj.time[-1],'ps'
+	print pca_traj.n_atoms, "atoms and ", pca_traj.n_residues, "residues in the trajectory"
+	
+	if options.reference == None: 
+		print "Reference for RMSD calculation is first frame of trajectory"
+	else:
+		print "Reference for RMSD calculation is: ", options.reference
+		
+	if atm_name == None:
+		print "Total", len(sele_grp), 'C-alpha atoms selected for analysis\n'
+	else:
+		print "Total", len(sele_grp), atm_name,'atoms selected for analysis\n'
+	return;
+
+	
+trajectory_info()
 
 #===============================================================
 #
 #  RMSD in reference with first frame
 #
 #===============================================================
-rmsd = md.rmsd(pca_traj, pca_traj, 0, atom_indices=sele_grp)
-print "RMSD written to rmsd.agr \n "
-## write the RMSD file
-np.savetxt('rmsd.agr', rmsd)
-rf = open('rmsd.agr', 'r')
-rf_cont = rf.read()
-rf.close()
 
-my_time = strftime("%Y-%m-%d  %a  %H:%M:%S", gmtime())
-title = '\tcreated by pca.py\t'
-legends = '@    title "RMSD"\n\
-@    xaxis  label "Time"\n\
-@    yaxis  label "RMSD"\n\
-@	TYPE xy\n'
-
-pf = open('rmsd.agr', 'w')
-pf.write('#'+title+'\ton\t'+my_time+'\n'+legends+'\n'+rf_cont)
-pf.close()
-
+def get_rmsd():
+	rmsd = md.rmsd(pca_traj, ref, 0, atom_indices=sele_grp)
+	print "RMSD written to rmsd.agr \n "
+	## write the RMSD file
+	np.savetxt('rmsd.agr', rmsd)
+	rf = open('rmsd.agr', 'r')
+	rf_cont = rf.read()
+	rf.close()
+	
+	my_time = strftime("%Y-%m-%d  %a  %H:%M:%S", gmtime())
+	title = '\tcreated by pca.py\t'
+	legends = '@    title "RMSD"\n\
+	@    xaxis  label "Time"\n\
+	@    yaxis  label "RMSD"\n\
+	@	TYPE xy\n'
+	
+	pf = open('rmsd.agr', 'w')
+	pf.write('#'+title+'\ton\t'+my_time+'\n'+legends+'\n'+rf_cont)
+	pf.close()
+	return;
+	
+get_rmsd()
 #===============================================================
 #
 # PCA using sci-kit learn library
