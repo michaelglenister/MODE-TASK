db0fa7cc44a9e00521a163aed81da6d6aec5f0e5
nizamibilal1064 <nizamibilal1064@gmail.com>
2017-08-09 10:33:12 +0200

    added cosine content, fixed number of components, user can pass type of MDS

--- mds.py
+++ mds.py
@@ -7,7 +7,7 @@ import numpy as np
 from sklearn.metrics import euclidean_distances
 from sklearn.manifold import MDS
 from write_plot import write_plots, write_pcs
-from traj_info import trajectory_info, get_internal_cordinates, get_trajectory
+from traj_info import trajectory_info, get_internal_cordinates, get_trajectory, get_cosine
 from welcome_msg import welcome_msg
 
 def main():
@@ -38,6 +38,7 @@ def get_options():
 	
 	parser.add_argument("-t", "--trj", dest="trj",				help="file name of the MD trajectory")
 	parser.add_argument("-p", "--top", dest="topology",				help="topology file")
+	parser.add_argument("-mt", "--mds_type", dest="mds_type",				help="Type of MDS. Options are nm=non-metric, metric=metric")
 	parser.add_argument("-dt", "--dissimilarity_type",  dest="dissimilarity_type",				help="Type of dissimilarity matrix to use. euc = Euclidean distance between internal cordinates, rmsd= pairwise RMSD. Default is rmsd")
 	parser.add_argument("-ag", "--ag", dest="atm_grp", help="group of atom for MDS. Default is C alpha atoms. Other options are :"				  "all= all atoms, backbone = backbone atoms, CA= C alpha atoms, protein= protein's atoms")	
 	parser.add_argument("-ict", "--cordinate_type",  dest="cordinate_type",				help="Internal cordinates type. Default is pairwise distance")
@@ -64,7 +65,13 @@ def get_options():
 		print 'ERROR: Missing toplogy argument.... :(  \nPlease see the help by running \n\nsystem_setup.py -h\n\n '
 		parser.print_help()
 		sys.exit(1)
-
+		
+	if args.mds_type == None:
+		print 'No MDS type given... pefroming non-metric MDS...'
+		args.mds_type='nm'
+	if args.mds_type not in  ('nm', 'metric', None):
+		print 'ERROR: no such option as', args.mds_type, 'for flag -mt \nPlease see the usage\n\n '
+		sys.exit(1)
 		
 	if args.cordinate_type not in  ('distance', 'phi', 'psi', None):
 		print 'ERROR: no such option as', args.cordinate_type, 'for flag -ct \nPlease see the usage\n\n '
@@ -130,25 +137,59 @@ def get_pair_rmsd(pca_traj, sele_grp):
 #
 #=============================================
 
-def mds(input):
+def mds(input, type):
 	'metric and nonmetric Multidimensional scaling'
 	seed = np.random.RandomState(seed=1)
 	
-	print "Performing metric MDS.."
-	mmds = MDS(max_iter=3000, random_state=seed, dissimilarity="precomputed")
-	print "Performing non-metric MDS.."
-	mpos = mmds.fit(input).embedding_
-	nmds=MDS(max_iter=3000, metric=False, random_state=seed, dissimilarity="precomputed")
-	npos=nmds.fit_transform(input, init=mpos)
-	write_plots('mmds_projection', mpos)
-	write_plots('nmds_projection', npos)
+	if type == 'nm':
+		nmds=MDS(n_components=100, max_iter=3000, metric=False, random_state=seed, dissimilarity="precomputed")
+		print "Performing non-metric MDS.."
+		npos=nmds.fit_transform(input)
+		
+		# write PC plots
+		write_plots('nmds_projection', npos)
+		
+		# cosine content 
+		
+		pc1_cos=get_cosine(npos, 0)
+		print 'cosine content of first PC=',pc1_cos
+		pc2_cos=get_cosine(npos, 1)
+		print 'cosine content of second PC=', pc2_cos
+		pc3_cos=get_cosine(npos, 2)
+		print 'cosine content of 3rd PC=',pc3_cos
+		pc4_cos=get_cosine(npos, 3)
+		print 'cosine content of 4th PC=', pc4_cos
+			
+	elif type == 'metric':
+		mmds = MDS(n_components=100, max_iter=3000, random_state=seed, dissimilarity="precomputed")
+		print "Performing metric MDS.."
+		mpos = mmds.fit_transform(input)
+		
+		# write PC plots
+		write_plots('mmds_projection', mpos)
+		
+		# cosine content 
+		
+		pc1_cos=get_cosine(mpos, 0)
+		print 'cosine content of first PC=',pc1_cos
+		pc2_cos=get_cosine(mpos, 1)
+		print 'cosine content of second PC=', pc2_cos
+		pc3_cos=get_cosine(mpos, 2)
+		print 'cosine content of 3rd PC=',pc3_cos
+		pc4_cos=get_cosine(mpos, 3)
+		print 'cosine content of 4th PC=', pc4_cos
+	
+	else:
+		print 'ERROR: Please check -mt flag options by running mds.py -h'
+	
 	return;
 
+type=args.mds_type
 
 if args.dissimilarity_type == 'rmsd' or args.dissimilarity_type == None:
 	print 'using pairwise RMSD...'
 	pair_rmsd=get_pair_rmsd(pca_traj, sele_grp)
-	mds(pair_rmsd)
+	mds(pair_rmsd, type)
 	print 'FINISHED!'
 if args.dissimilarity_type == 'euc':
 	if args.cordinate_type == None:
@@ -157,7 +198,7 @@ if args.dissimilarity_type == 'euc':
 	print 'using Euclidean distance between', atom_indices
 	int_cord=get_internal_cordinates(top, args.cordinate_type, pca_traj, atom_indices)
 	similarities = euclidean_distances(int_cord)
-	mds(similarities)
+	mds(similarities, type)
 	print 'FINISHED!'
 
 
