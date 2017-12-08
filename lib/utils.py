d8e5c2deb923841b320420940f5c113e396fa85c
olivier <oliserand@gmail.com>
2017-12-08 12:34:43 +0200

    Updated utils.py for python3
    Added brackets to print statements

--- lib/utils.py
+++ lib/utils.py
@@ -8,22 +8,21 @@ def format_seconds(seconds):
     h, m = divmod(m, 60)
     return "%d:%02d:%02d" % (h, m, s)
 
-
 def welcome_msg(title, author):
 	'Print Welcome message'
-	print '============================================================'
-	print '\t\t\t\t\t\t\t\t'
-	print '\t:-) >>------->MODE-TASK<-------<< (-:\t'
-	print '\t\t\t\t\t\t\t\t'
-	print '\t   :-) >>--->',title,'<---<< (-:\t'
-	print '\t\t\t\t\t\t\t\t'
-	print '\t\t\t\t\t\t\t\t'
-	print '\tAuthor(s):', author,'\t\t\t\t'
-	print '\tResearch Unit in Bioinformatics (RUBi)\t\t'
-	print '\tRhodes University, 2017\t\t\t\t'
-	print '\tDistributed under GNU GPL 3.0\t\t\t'
-	print '\t\t\t\t\t\t\t\t'
-	print '\thttps://github.com/michaelglenister/NMA-TASK\t'
-	print '\t\t\t\t\t\t\t\t'
-	print '============================================================'
+	print('============================================================')
+	print('\t\t\t\t\t\t\t\t')
+	print('\t:-) >>------->MODE-TASK<-------<< (-:\t')
+	print('\t\t\t\t\t\t\t\t')
+	print('\t   :-) >>--->',title,'<---<< (-:\t')
+	print('\t\t\t\t\t\t\t\t')
+	print('\t\t\t\t\t\t\t\t')
+	print('\tAuthor(s):', author,'\t\t\t\t')
+	print('\tResearch Unit in Bioinformatics (RUBi)\t\t')
+	print('\tRhodes University, 2017\t\t\t\t')
+	print('\tDistributed under GNU GPL 3.0\t\t\t')
+	print('\t\t\t\t\t\t\t\t')
+	print('\thttps://github.com/michaelglenister/NMA-TASK\t')
+	print('\t\t\t\t\t\t\t\t')
+	print('============================================================')
 	return;
